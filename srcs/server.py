2eb165719a556c5015f87802d0bafda43475fcbb
yungKram <markartyukh@gmail.com>
2023-09-04 15:29:03 +0200

    fix

--- srcs/server.py
+++ srcs/server.py
@@ -12,10 +12,11 @@ logger = logging.getLogger('prom') if os.getenv('ENV') == 'prom' else logging.ge
 
 WEB_HOOK_PATH = '/web_hook'
 WEB_HOOK_URL = os.getenv('DOMAIN_NAME', 'http://192.168.1.90') + WEB_HOOK_PATH
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 app = FastAPI()
 chain = myChain()
-bot = TelegramChainBot(os.getenv('TELEGRAM_BOT_TOKEN'), WEB_HOOK_URL, int(os.getenv('PORT')), chain)
+bot = TelegramChainBot(TELEGRAM_TOKEN, WEB_HOOK_URL, int(os.getenv('PORT')), chain)
 
 
 
@@ -29,7 +30,8 @@ async def webhook(data: Update):
     logging.info(data)
     logging.info("web_hook request recieved")
     text = data.message.text
-    status_code = bot.chain.handle_question(text)
+    response_text = bot.chain.handle_question(text)
+    status_code = send_message(TELEGRAM_TOKEN, data.chat.id, response_text)
 
     return Response(status_code=status_code)
 
